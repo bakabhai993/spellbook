4349995dc26e9418dedeaf533b040aa3825fcf40
Deepanjal Gupta <guptadeepanjal01@gmail.com>
2022-12-05 14:49:32 +0530

    sql and schema file changes

--- models/zeroex/ethereum/deduped_fills/zeroex_api_ethereum_deduped_fills.sql
+++ models/zeroex/ethereum/deduped_fills/zeroex_api_ethereum_deduped_fills.sql
@@ -1,18 +1,38 @@
+{{  config(
+        alias='fills_deduped',
+        materialized='incremental',
+        partition_by = ['block_date'],
+        unique_key = ['block_date', 'tx_hash', 'evt_index'],
+        on_schema_change='sync_all_columns',
+        file_format ='delta',
+        incremental_strategy='merge',
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "project",
+                                "zeroex",
+                                \'["sui414", "bakabhai993"]\') }}'
+    )
+}}
+
+{% set zeroex_v3_start_date = '2019-12-01' %}
+--the code to add the data into the dex.trades table.
+--this code is also the deduped version for the fills tables.
+--only the data for 0x API fills.
+--dependent on:zeroex_ethereum.fills
 
 WITH fills_with_tx_fill_number
 AS
 (
     SELECT   row_number() OVER ( partition BY tx_hash ORDER BY evt_index ASC ) AS tx_fill_number
            , *
-      FROM zeroex_ethereum.fills
+      FROM {{ ref('zeroex_api_ethereum_fills') }} 
     WHERE 1=1
-    -- AND tx_hash IN ( 
-    --                  '0xfebc6867d857bf727e1d9f5e7ef58bf2c562c64015e1901c2524652fedea513a'
-    --                 ,'0x27f1de5f9eef84e3ea5a92d8802162e7ef6c5a0298fee2251385cbd805a0e04c'
-    --                 ,'0x01c555264a5b79956b28e00c9392ff4b397e43f7abad76d33c2630317c933a01'
-    --                 )
-    AND block_date = '2022-08-09'
     AND swap_flag = 1
+    {% if is_incremental() %}
+    AND block_time >= date_trunc('day', now() - interval '1 week')
+    {% endif %}
+    {% if not is_incremental() %}
+    AND block_time >= '{{zeroex_v3_start_date}}'
+    {% endif %}    
 )
 , fills_first_last
 AS
@@ -25,67 +45,52 @@ AS
       FROM  fills_with_tx_fill_number a 
             LEFT JOIN fills_with_tx_fill_number b ON (a.tx_hash = b.tx_hash AND a.tx_fill_number = b.tx_fill_number - 1)
             LEFT JOIN fills_with_tx_fill_number c ON (a.tx_hash = c.tx_hash AND a.tx_fill_number = c.tx_fill_number + 1)    
-)    
--- SELECT * 
---  FROM fills_first_last
--- ORDER BY evt_index ASC
+)
 , deduped_bridge_fills
 AS
 (
-    SELECT 
-              tx_hash
-            , MAX(evt_index)                                                               AS evt_index
-            , MAX(affiliate_address)                                                       AS affiliate_address
-            , MAX(CASE WHEN taker_consider_flag = 0 THEN NULL ELSE taker_token END )       AS taker_token
-            , MAX(CASE WHEN maker_consider_flag = 0 THEN NULL ELSE maker_token END )       AS maker_token
-            , MAX(CASE WHEN taker_consider_flag = 0 THEN NULL ELSE taker_token_amount END)   AS taker_token_amount
-            , MAX(CASE WHEN maker_consider_flag = 0 THEN NULL ELSE maker_token_amount END)   AS maker_token_amount
-            , MAX(CASE WHEN taker_consider_flag = 0 THEN NULL ELSE taker_token_amount_raw END)   AS taker_token_amount_raw
-            , MAX(CASE WHEN maker_consider_flag = 0 THEN NULL ELSE maker_token_amount_raw END)   AS maker_token_amount_raw
-            , COUNT(*)                                                                     AS fills_within
+    SELECT   tx_hash
+        , MAX(evt_index)                                                                    AS evt_index
+        , MAX(affiliate_address)                                                            AS affiliate_address
+        , MAX(CASE WHEN taker_consider_flag = 0 THEN NULL ELSE taker_token END )            AS taker_token
+        , MAX(CASE WHEN maker_consider_flag = 0 THEN NULL ELSE maker_token END )            AS maker_token
+        , MAX(CASE WHEN taker_consider_flag = 0 THEN NULL ELSE taker_symbol END )           AS taker_symbol
+        , MAX(CASE WHEN maker_consider_flag = 0 THEN NULL ELSE maker_symbol END )           AS maker_symbol        
+        , MAX(CASE WHEN taker_consider_flag = 0 THEN NULL ELSE taker_token_amount END)      AS taker_token_amount
+        , MAX(CASE WHEN maker_consider_flag = 0 THEN NULL ELSE maker_token_amount END)      AS maker_token_amount
+        , MAX(CASE WHEN taker_consider_flag = 0 THEN NULL ELSE taker_token_amount_raw END)  AS taker_token_amount_raw
+        , MAX(CASE WHEN maker_consider_flag = 0 THEN NULL ELSE maker_token_amount_raw END)  AS maker_token_amount_raw
+        , COUNT(*)                                                                          AS fills_within
     FROM fills_first_last a
-    GROUP BY tx_hash,hop_count             
+    GROUP BY  tx_hash,hop_count
 )
 SELECT  'ethereum' AS blockchain
       , '0x API' AS project 
       , '' AS version
       , a.block_date
       , a.block_time
-      , COALESCE(c.symbol,b.taker_token) AS token_bought_symbol
-      , COALESCE(d.symbol,b.maker_token) AS token_sold_symbol
-      , CASE WHEN lower(c.symbol) > lower(d.symbol) then concat(d.symbol, '-', c.symbol)
-             ELSE concat(c.symbol, '-', d.symbol)
-             END AS token_pair
-      , b.taker_token_amount AS token_bought_amount
-      , b.maker_token_amount AS token_sold_amount
-      , b.taker_token_amount_raw AS token_bought_amount_raw
-      , b.maker_token_amount_raw AS token_sold_amount_raw    
+      --what token did taker give
+      , COALESCE(b.taker_symbol,b.taker_token) AS taker_symbol
+      --what token did maker give
+      , COALESCE(b.maker_symbol,b.maker_token) AS maker_symbol
+      , CASE WHEN lower(b.taker_symbol) > lower(b.maker_symbol) THEN concat(b.maker_symbol, '-', b.taker_symbol) ELSE concat(b.taker_symbol, '-', b.maker_symbol) END AS token_pair
+      , b.taker_token_amount
+      , b.maker_token_amount
+      , b.taker_token_amount_raw
+      , b.maker_token_amount_raw
       , a.volume_usd AS amount_usd
-      , b.taker_token AS token_bought_address
-      , b.maker_token AS token_sold_address
+      , b.taker_token
+      , b.maker_token
       , a.taker
       , a.maker
       , a.affiliate_address AS project_contract_address
       , a.tx_hash
-      , e.tx_from
-      , e.tx_to
+      , a.tx_from
+      , a.tx_to
       , '' AS trace_address
       , b.evt_index
       , '' AS unique_trade_id
   FROM fills_with_tx_fill_number a 
     JOIN deduped_bridge_fills b ON (a.tx_hash = b.tx_hash AND a.evt_index = b.evt_index)
-    LEFT JOIN tokens.erc20 c ON (c.contract_address = b.taker_token AND c.blockchain = 'ethereum')
-    LEFT JOIN tokens.erc20 d ON (d.contract_address = b.maker_token AND c.blockchain = 'ethereum')
-    LEFT JOIN ( SELECT from AS tx_from, to AS tx_to, hash AS tx_hash  FROM ethereum.transactions ) e ON (a.tx_hash = e.tx_hash)
 ORDER BY  a.tx_hash,a.evt_index
 ;
---token bought is a
---token sold is b
---a taker
---b maker
---SELECT * FROM dex_aggregator.trades LIMIT 5
-
---SELECT * FROM dex_aggregator.trades LIMIT 5
---token bought is a is taker
---token sold is b is maker
---SELECT * FROM tokens.erc20 LIMIT 5
